91739861f3fa46d43bb8422377fe57de0b5e88bd
AchyutTiwari21 <achyut.s.tiwari@gmail.com>
2024-10-04 16:42:17 +0530

    utilsSetup

--- src/index.js
+++ src/index.js
@@ -6,7 +6,16 @@ dotenv.config({
     path: './env'
 });
 
-connectDB();
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running at port : ${process.env.PORT}`);
+    })
+})
+.catch((err) => {
+    console.log("MONGO DB connection failed !!!", err);
+    
+})
 
 
 
@@ -21,10 +30,10 @@ const app = e();
 ( async () => {
     try {
         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
-        app.on("error", (error) => {
-            console.log("ERR: ", error);   
-            throw error;      
-        });
+        // app.on("error", (error) => {
+        //     console.log("ERR: ", error);   
+        //     throw error;      
+        // });
 
         app.listen(process.env.PORT, () => {
             console.log(`App is listening on port ${process.env.PORT}`);
