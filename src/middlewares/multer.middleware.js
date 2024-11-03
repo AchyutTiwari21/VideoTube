593525e24a0f7abcc010fe78d0edc1f28524f5f7
AchyutTiwari21 <achyut.s.tiwari@gmail.com>
2024-11-03 13:22:58 +0530

    Tested and Modified Register User

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -4,11 +4,11 @@ const storage = multer.diskStorage({
     destination: function (req, file, cb) {
       cb(null, "./public/temp")
     },
-    filename: function (req, file, cb) {
+    filename: function (req, file, cb) {  
       cb(null, file.originalname)
     }
-})
+});
   
-export const upload = multer({
-    storage,
+export const upload = multer({ 
+    storage, 
 });
--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -13,15 +13,16 @@ const uploadOnCloudinary = async (localFilePath) => {
     try {
         if(!localFilePath) return null;
         //upload the file on clodinary
-        const response = await clodinary.uploader.upload(localFilePath, {
+        const response = await cloudinary.uploader.upload(localFilePath, {
             resource_type: "auto"
         });
         // file has been uploaded successfully
-        console.log("file is uploaded on cloundinary ", response.url);
+        // console.log("Cloudinary Response: ", response);
+        // console.log("file is uploaded on cloundinary ", response.url);
+        fs.unlinkSync(localFilePath);
         return response;
-
     } catch (error) {
-        fs.unlinkSync(localFilePath) // remove the locally saved temporary file as the upload operation got failed
+        fs.unlinkSync(localFilePath); // remove the locally saved temporary file as the upload operation got failed
         return null;
     }
 }
